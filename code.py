70ddb1857a212da5ce7e32fa2afb14a9804cbaa6
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-12-11 13:01:14 -0500

    Added code.py

---
+++ code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Tony Tran
+# Date: December. 11, 2023
+# This program is the "Space Aliens" program on the PyBadge
+
+import stage
+import ugame
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n")
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
