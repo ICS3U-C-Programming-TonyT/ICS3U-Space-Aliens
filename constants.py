83d091d11a15596b692278c8732ac56f8307c0fd
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-12-14 12:38:12 -0500

    Modified code.py | constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Created By: Tony Tran
+# Date: December. 14, 2023
+# This constants file is for Space Alien game
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
