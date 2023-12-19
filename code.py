6a49a4394e3c5c28e07dfcbe173edc3a31683d9f
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-12-19 13:11:42 -0500

    Modified code.py

--- code.py
+++ code.py
@@ -3,38 +3,47 @@
 # Date: December. 11, 2023
 # This program is the "Space Aliens" program on the PyBadge
 
+import constants
 import stage
 import ugame
-import constants
+
 
 def menu_scene_2():
     # this function is the menu scene
 
     # image banks for CircuitPython
     image_bank_mt_background = stage.Bank.from_bmp16("images\mt_game_studio.bmp")
-    
+
     # Settings
     select = "start"
 
     # add text objects
     text = []
-    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1 = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     text1.move(20, 10)
     text1.text("Capybara Catcher")
     text.append(text1)
 
-    start_text = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    start_text = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     start_text.move(40, 60)
     start_text.text("> START <")
     text.append(start_text)
 
-    instruction_text = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    instruction_text = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     instruction_text.move(40, 90)
     instruction_text.text("INSTRUCTIONS")
     text.append(instruction_text)
 
     # sets the background to image 0 in the image bank
-    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y)
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
@@ -48,8 +57,8 @@ def menu_scene_2():
     while True:
         # settings options
         options = {
-            1:"start",
-            2:"instructions",
+            1: "start",
+            2: "instructions",
         }
         # get user input
         keys = ugame.buttons.get_pressed()
@@ -67,42 +76,50 @@ def menu_scene_2():
                 select = options[2]
                 instruction_text.text("> INSTRUCTIONS <")
                 start_text.text("START")
-                
-        
+
         if keys & ugame.K_START != 0:
             game_scene()
 
         # redraw sprites
         game.tick()
+
 
 def menu_scene_1():
     # this function is the menu scene
 
     # image banks for CircuitPython
     image_bank_mt_background = stage.Bank.from_bmp16("images\mt_game_studio.bmp")
-    
+
     # Settings
     select = "start"
 
     # add text objects
     text = []
-    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1 = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     text1.move(20, 10)
     text1.text("Capybara Catcher")
     text.append(text1)
 
-    start_text = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    start_text = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     start_text.move(40, 60)
     start_text.text("> START <")
     text.append(start_text)
 
-    instruction_text = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    instruction_text = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     instruction_text.move(40, 90)
     instruction_text.text("INSTRUCTIONS")
     text.append(instruction_text)
 
     # sets the background to image 0 in the image bank
-    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y)
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
@@ -116,8 +133,8 @@ def menu_scene_1():
     while True:
         # settings options
         options = {
-            1:"start",
-            2:"instructions",
+            1: "start",
+            2: "instructions",
         }
         # get user input
         keys = ugame.buttons.get_pressed()
@@ -135,13 +152,13 @@ def menu_scene_1():
                 select = options[2]
                 instruction_text.text("> INSTRUCTIONS <")
                 start_text.text("START")
-                
-        
+
         if keys & ugame.K_START != 0:
             game_scene()
 
         # redraw sprites
         game.tick()
+
 
 def game_scene():
     # this function is the main game game_scene
@@ -149,7 +166,7 @@ def game_scene():
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("images\space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("images\sprites\space_aliens.bmp")
-    
+
     # buttons that you want to keep state information on
 
     a_button = constants.button_state["button_up"]
@@ -165,10 +182,14 @@ def game_scene():
 
     # set the background to image 0 in the image bank
     #   and the size (10x8 tiles of size 16x16)
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
 
     # a sprite that will be updated every frame
-    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2*constants.SPRITE_SIZE))
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
@@ -181,7 +202,6 @@ def game_scene():
 
     # repeat forever, game loop
     while True:
-
         keys = ugame.buttons.get_pressed()
 
         if keys & ugame.K_X != 0:
@@ -214,12 +234,13 @@ def game_scene():
             pass
         if keys & ugame.K_DOWN != 0:
             pass
-        
+
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
 
         game.render_sprites([ship])
         game.tick()
+
 
 if __name__ == "__main__":
     menu_scene_1()
